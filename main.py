29b64c0ac1b2040d5204fef08baeee76964d90e8
Ryan <ryanleonbutler@gmail.com>
2019-04-02 22:35:27 +0200

    2019-04-02 22:35:27

--- main.py
+++ main.py
@@ -1,63 +1,77 @@
 def transform(letter):
-    for n in text:
+    letters = []
+    a = """
+      _
+     /_\\
+    /   \\
+    """
+    a = a.rstrip()
+
+    b = """
+     __
+    |__]
+    |__]
+    """
+    b = b.rstrip()
+
+    c = """
+     __
+    |
+    |__
+    """
+    c = c.rstrip()
+
+    d = """
+     __
+    |  \\
+    |__/
+    """
+    d = d.rstrip()
+
+    e = """
+     __
+    |__
+    |__
+    """
+    e = e.rstrip()
+
+    f = """
+     __
+    |__
+    |
+    """
+    f = f.rstrip("")
+
+    newtext = ''
+    for n in letter:
         if n.lower() == "a":
             n == a
             newtext += a
         elif n.lower() == "b":
             n == b
             newtext += b
+        elif n.lower() == "c":
+            n == c
+            newtext += c
+        elif n.lower() == "d":
+            n == d
+            newtext += d
+        elif n.lower() == "e":
+            n == e
+            newtext += e
+        elif n.lower() == "f":
+            n == f
+            newtext += f
+
+    return newtext
+
 
 letters = []
-a = """
-  _
- /_\\
-/   \\
-"""
-a = a.rstrip()
-letters.append(a)
+input_text = input("Please enter text:")
+input_text = input_text.split()
 
-b = """
- __
-|__]
-|__]
-"""
-b = b.rstrip()
-letters.append(b)
+for letter in input_text:
+    letters.append(transform(letter))
 
-c = """
- __
-|
-|__
-"""
-c = c.rstrip()
-letters.append(c)
-
-d = """
- __
-|  \\
-|__/
-"""
-d = d.rstrip()
-letters.append(d)
-
-e = """
- __
-|__
-|__
-"""
-e = e.rstrip()
-letters.append(e)
-
-f = """
- __
-|__
-|
-"""
-f = f.rstrip()
-letters.append(f)
-
-newtext = ''
-text = ''
-text = input("Please enter text:")
-
-print(newtext)
+for item in letters:
+    print(item)

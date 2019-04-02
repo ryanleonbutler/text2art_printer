f8902d61ffac70367b34be8e8710f1b885ae74bc
Ryan <ryanleonbutler@gmail.com>
2019-04-02 16:15:56 +0200

    2019-04-02 16:15:56

---
+++ main.py
@@ -0,0 +1,63 @@
+def transform(letter):
+    for n in text:
+        if n.lower() == "a":
+            n == a
+            newtext += a
+        elif n.lower() == "b":
+            n == b
+            newtext += b
+
+letters = []
+a = """
+  _
+ /_\\
+/   \\
+"""
+a = a.rstrip()
+letters.append(a)
+
+b = """
+ __
+|__]
+|__]
+"""
+b = b.rstrip()
+letters.append(b)
+
+c = """
+ __
+|
+|__
+"""
+c = c.rstrip()
+letters.append(c)
+
+d = """
+ __
+|  \\
+|__/
+"""
+d = d.rstrip()
+letters.append(d)
+
+e = """
+ __
+|__
+|__
+"""
+e = e.rstrip()
+letters.append(e)
+
+f = """
+ __
+|__
+|
+"""
+f = f.rstrip()
+letters.append(f)
+
+newtext = ''
+text = ''
+text = input("Please enter text:")
+
+print(newtext)
